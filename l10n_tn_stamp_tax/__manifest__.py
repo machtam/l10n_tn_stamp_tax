08f5b122e525271f5b75e644c79adbe5f3690a1e
Mohamed <contact@netformica.com>
2020-11-13 08:47:40 +0100

    Add license and cover image

--- l10n_tn_stamp_tax/__manifest__.py
+++ l10n_tn_stamp_tax/__manifest__.py
@@ -1,3 +1,6 @@
+# Copyright Netformica - Mohamed Machta
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
 {
     'name': "Tunisia Stamp Tax",
     'version': '14.0.0.2',
@@ -9,5 +12,8 @@
     'data': [
         'views/invoice.xml',
         'views/account_tax_view.xml',
+    ],
+    'images': [
+        'static/description/screen.jpg',
     ],
 }
